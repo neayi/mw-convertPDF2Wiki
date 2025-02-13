9c43106c9dbcdea4931f55a512af50638da70d45
Bertrand Gorge <bertrand.gorge@neayi.com>
2025-02-13 15:08:27 +0100

    Follow redirects

--- md2wiki.php
+++ md2wiki.php
@@ -70,6 +70,7 @@ function getOpenGraphTitleFromURL($url)
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
         curl_setopt($ch, CURLOPT_TIMEOUT, 7);
         $html = curl_exec($ch);
         curl_close($ch);
